cc68101e0d4dac78fbffd315366e4f4d0e03c41e
igorevcarvalho <igorevcarvalho@outlook.com>
2025-12-30 18:17:54 +0000

    Abou the GetAll method... it's functional but can be improved by: - making it return an IQueryable to improve escalability. - adding pagination and filter capabilities, to optimize bandwidth usage and overall availability of both application and database servers. - replacing .ToList() with .AsNoTracking(), to reduce data transfer

--- MovieRental/Movie/MovieFeatures.cs
+++ MovieRental/Movie/MovieFeatures.cs
@@ -1,4 +1,5 @@
-﻿using MovieRental.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using MovieRental.Data;
 
 namespace MovieRental.Movie
 {
@@ -18,11 +19,9 @@ namespace MovieRental.Movie
 		}
 
 		// TODO: tell us what is wrong in this method? Forget about the async, what other concerns do you have?
-		public List<Movie> GetAll()
+		public IQueryable<Movie> GetAll()
 		{
-			return _movieRentalDb.Movies.ToList();
+			return _movieRentalDb.Movies.AsNoTracking();
 		}
-
-
 	}
 }
